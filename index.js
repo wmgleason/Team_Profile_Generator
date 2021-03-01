b014590f7ec905f7140b1b1dffede8b6bb2e94b0
Wanda Gleason <oglissane@gmail.com>
2021-02-28 21:26:49 -0500

    Working on breaking larger function into component functions to make it easier to debug.

--- index.js
+++ index.js
@@ -1,135 +1,164 @@
-
-let inquirer = require('inquirer');
-var fs = require('fs');
+const inquirer = require("inquirer");
+var fs = require("fs");
 const jest = require("jest");
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
-var ID = 0;
-var teamArray = [];
+var teamProfileArray = [];
 
-//index.js
-
-// let fs = require('fs');
-const util = require('util');
-// const generateHTML = require('./src/generateHTML');
+const util = require("util");
+// const teamProfile = require('./src/teamProfile.html');
 // const promisify = require('util.promisify');
 
 // TODO: Create an array of questions for user input
 // const writeFileAsync = util.promisify(fs.writeFile);
 
-// fs.writeFile('renderedTeamProfile.html', 'Hello content!', function (err) {
+// fs.writeFile('teamProfile.html', 'Hello content!', function (err) {
 //   if (err) throw err;
 //   console.log('Saved!');
 // });
 
-
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
-
-function promptUser (answers)  {
-    return inquirer.prompt([
+function promptUser ()  {
+    inquirer.prompt([
         {
             type: "list",
             name: "role",
-            message: "what is the employee's role? Please select one using the up or down arrow keys and hit enter.",
+            message: "Please select the employee's role using the up or down arrow keys and hit enter.",
             choices: ["Engineer", "Intern", "Manager"]
-        },
-    ]).then(function (res) {
-        console.log(res)
-        if (res.role === "Manager") {
-            inquirer.prompt([
-                {
-                    name: "name",
-                    message: "What is the employee's name?",
-                    type: "input"
-                },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is the employee's email?"
-                },
-                {
-                    name: "officeNumber",
-                    type: "input",
-                    message: "What is the manager's office number?"
-                }
-            ]).then(function (managerRes) {
+        }
+        ])
+            .then(function (res) {
+            // console.log(res);
+                if (res.role === "Manager") 
+                addManager();
+            })
+}
+    function addManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "Name",
+                message: "What is the manager's name?",
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "Enter the manager's employee ID?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the manager's email address?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the manager's office number?"
+            },
+        ])
+            .then(function (managerRes) {
                 var newManager = new Manager(managerRes.name, managerRes.email, ID, managerRes.officeNumber);
-                ID = ID + 1; // could be "ID++"
                 console.log(newManager);
-                teamArray.push(newManager);
-                addUser();
-                
+                teamProfileArray.push(newManager);
+                addEmployee();
             });
-        } else if (res.role === "Engineer") {
-            inquirer.prompt([
-            {
-                type: "input",
-                message: "Enter the engineer's name, please:",
-                name: "Name"
-            },
-            {
-                type: "input",
-                message: "Enter the engineer's employee ID:",
-                name: "ID"
-            },
-            {
-                type: "input",
-                message: "Enter the engineer's email address:",
-                name: "Email"
-            },
-        {
-            type: "input",
-            message: "Enter the engineer's GitHub Username:",
-            name: "GitHub"
-        }
-    ]).then(function (engineerRes) {
-        var newEngineer = new Engineer(engineerRes.name, engineerRes.email, ID, engineerRes.github);
-        ID = ID + 1; // could be "ID++"
-        console.log(newEngineer);
-        // run promptUser (called recursion) so that you can add multiple Engineers and id changes incrementally
-        teamArray.push(newEngineer);
-        addUser();
-        
-    });
-} else if (res.role === "Intern") {
-    inquirer.prompt([
-        {
-            name: "name",
-            message: "What is the intern's name?",
-            type: "input"
-        },
-        {
-            name: "email",
-            type: "input",
-            message: "What is the intern's email address?"
-        },
-        {
-            name: "school",
-            type: "input",
-            message: "Where did the intern attend college/university?"
-        }
-    ]).then(function (internRes) {
-        var newIntern = new Intern(internRes.name, internRes.email, ID, internRes.school);
-        ID = ID + 1; // could be "ID++"
-        console.log(newIntern)
-        teamArray.push(newIntern);
-        addUser();
-    });
-    };
-})
-    .catch(function (err) {
-        console.log(err);
-    });
+            function addEmployee() {
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        message: "Would you like to add an additional team member?",
+                        choices: ["Yes, add an engineer", "Yes, add an intern", "No, my team is complete"],
+                        name: "addEmployeeRole"
+                    }
+                ])
+            
+                    .then(function (res) {
+            
+                        switch (res.addEmployeeRole) {
+                            case "Yes, please add an engineer.":
+                                addEngineer();
+                                break;
+            
+                            case "Yes, please add an intern.":
+                                addIntern();
+                                break;
+                            case "No, my team is perfect.":
+                                compileTeam();
+                                break;
+                        }
+                    });
+            }
+            if (res.role === "Engineer") {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "Name",
+                        message: "Enter the engineer's name, please:"
+                    },
+                    {
+                       type: "input",
+                        name: "ID",
+                        message: "Enter the engineer's employee ID:"
+                    },
+                    {
+                        type: "input",
+                        name: "Email",
+                        message: "Enter the engineer's email address:"
+                    },
+                    {
+                       type: "input",
+                        name: "GitHub",
+                       message: "Enter the engineer's GitHub Username:"
+                    }
+                ]).then(function (engineerRes) {
+                    var newEngineer = new Engineer(engineerRes.name, engineerRes.email, ID, engineerRes.github);
+                    ID = ID;
+                    console.log(newEngineer);
+                    teamProfileArray.push(newEngineer);
+                    addEmployee();  
+                });
+            }
+    
+            else (res.role === "Intern") {
+                inquirer.prompt([
+                    {
+                        name: "name",
+                        message: "What is the intern's name?",
+                        type: "input"
+                    },
+                    {
+                        name: "email",
+                        type: "input",
+                        message: "What is the intern's email address?"
+                    },
+                    {
+                        name: "school",
+                        type: "input",
+                        message: "Where did the intern attend college/university?"
+                    }   
+                ]).then(function (internRes) {
+                    var newIntern = new Intern(internRes.name, internRes.email, ID, internRes.school);
+                    ID = ID;
+                    console.log(newIntern);
+                    teamProfileArray.push(newIntern);
+                    addEmployee();
+                });
+            }
+        // .catch(function (err) {
+        // console.log(err);
+        // });
 
-};
-promptUser();
+        function addEmployee(){
+        inquirer.prompt([
+            {   
+                name: "continue",
+                message: "Do you want to add another employee to your team?",
+                type: "confirm"
+            }
+        ]).then(function(confirmRes){
+            confirmRes.continue ? promptUser() : teamProfileArray();
+        });
+        }
+    },
+}
+// promptUser();
